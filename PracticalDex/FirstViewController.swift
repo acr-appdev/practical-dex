d91e1183018a726795849d25427af61a6ea0005b
Allan Clipes Rosa <allanccrosa@gmail.com>
2020-07-10 17:15:40 -0300

    Initial Commit

---
+++ PracticalDex/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PracticalDex
+//
+//  Created by Allan Rosa on 10/07/20.
+//  Copyright © 2020 Allan Rosa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
